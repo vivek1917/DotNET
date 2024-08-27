ce413cd94aa4b07ee4e4885208df969dea155f56
Vivek <vivek@44>
2024-08-27 14:47:25 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+
+    abstract class Shape{
+        String Shape_Name { get; set;}
+
+        public abstract void GetArea();
+
+    }
+
+    class Circle : Shape
+    {
+        public override void GetArea()
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Circle co = new Circle();
+            co.GetArea();
+        }
+    }
+}
